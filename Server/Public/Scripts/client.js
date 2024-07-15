af932fc61122fdb311fafbb90661fa15d0134583
Meleah Roe <melroe19@gmail.com>
2024-07-15 11:23:09 -0500

    added onsubmit event

--- Server/Public/Scripts/client.js
+++ Server/Public/Scripts/client.js
@@ -1,3 +1,7 @@
 console.log('well hello there');
+function onSubmit(event){
+    event.preventDefault()
+    console.log('onsubmit', event);
+    }
 let firstNameEntry = document.getElementById(`firstName`).value;
 console.log('first name is ', firstNameEntry);
