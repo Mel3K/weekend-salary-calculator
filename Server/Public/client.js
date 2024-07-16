2cdd8ce5c3a68b5c347edbe374e37b230649533b
Meleah Roe <melroe19@gmail.com>
2024-07-15 20:00:04 -0500

    making table

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -1,23 +1,38 @@
 console.log('well hello there');
+
+
+
 function submitForm(event){
     event.preventDefault();
-let new 
+// let newDiv = {
+//     FirstName: document.getElementById('fName').value,
+//    lastName: document.getElementById('lName').value,
+//    employeeId: document.getElementById('eID').value,
+//    title: document.getElementById('eTitle').value,
+//   salary: document.getElementById('salary').value,
+//     // console.log('in submitForm', FirstName, lastName, employeeId, title, salary);
+// }
+//let newTableEntry =  document.createElement('div');
 
-// let FirstName = document.getElementById('fName').value;
-// let lastName = document.getElementById('lName').value;
-// let employeeId = document.getElementById('eID').value;
-// let title = document.getElementById('eTitle').value;
-// let salary = document.getElementById('salary').value;
-// console.log('in submitForm', FirstName, lastName, employeeId, title, salary);
-
-};
+let firstName = document.getElementById('fName').value;
+let lastName = document.getElementById('lName').value;
+let employeeId = document.getElementById('eID').value;
+let title = document.getElementById('eTitle').value;
+let salary = document.getElementById('salary').value;
+console.log('in submitForm', firstName, lastName, employeeId, title, salary);
 
 
 
+function addNewElement(){
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('newDiv')
+    newDiv.textContent = "newStuffHere";
+    document.querySelector('.container').appendChild(newDiv);
+}
+} 
+// event.target.innerHTML = lastName;
+// event.target.innerHTML = employeeId;
+// event.target.innerHTML = title;
+// event.target.innerHTML = salary;
 
-  function handleClick(event) {
-    let newDiv = event.target.getElementById('fName').value;
 
-    console.log(newDiv);
-    newDiv.style.color = 'orange';
-  }
