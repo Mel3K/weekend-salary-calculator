5817dddb8d52ef8fa26bce2afd9aa9bd5cf51000
Meleah Roe <melroe19@gmail.com>
2024-07-15 19:14:01 -0500

    made new elements for items to put on dom

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -1,8 +1,23 @@
 console.log('well hello there');
 function submitForm(event){
     event.preventDefault();
-   
-document.getElementById()
+let new 
 
-console.log(newEntry);
-}
+// let FirstName = document.getElementById('fName').value;
+// let lastName = document.getElementById('lName').value;
+// let employeeId = document.getElementById('eID').value;
+// let title = document.getElementById('eTitle').value;
+// let salary = document.getElementById('salary').value;
+// console.log('in submitForm', FirstName, lastName, employeeId, title, salary);
+
+};
+
+
+
+
+  function handleClick(event) {
+    let newDiv = event.target.getElementById('fName').value;
+
+    console.log(newDiv);
+    newDiv.style.color = 'orange';
+  }
